c58a66c76151bbf026b10021fc07fcb7954302f0
Yan Esteves <yan.m.esteves@gmail.com>
2026-05-12 20:37:31 -0300

    Aula 2 - Semana 5 - Parte 1

---
+++ S05/Aula2/heranca.js
@@ -0,0 +1,30 @@
+class Animal {
+    nomeAnimal = ''
+    constructor(nome) {
+        this.nomeAnimal = nome
+    }
+
+    comer() {
+        console.log("O animal está comendo")
+    }
+}
+
+class Cachorro extends Animal {       
+    latir() {
+        console.log(`O ${this.nomeAnimal} está latindo`)
+    }
+
+    comer() {
+       console.log("O cachorro está comendo ração") 
+    }
+}
+
+class Gato extends Animal {
+    miar() {
+        console.log("O gato está miando")
+    }
+}
+
+const cachorro = new Cachorro('Buzzy')
+cachorro.latir()
+cachorro.comer()
