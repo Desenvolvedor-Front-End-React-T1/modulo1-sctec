050022de1fc6dbcd5eb361d3f2ad64a55d0e19b7
Yan Esteves <yan.m.esteves@gmail.com>
2026-06-02 21:02:58 -0300

    Exemplo com o chat e mudanca de cores com classList

---
+++ S08/aula2/chat/main.js
@@ -0,0 +1,25 @@
+const btnLaranja = document.getElementById('btnLaranja');
+const btnAzul = document.getElementById('btnAzul');
+const btnVerde = document.getElementById('btnVerde');
+
+const userQuestion = document.querySelector('.user-question')
+
+let corAplicada = 'orange'
+
+btnLaranja.addEventListener('click', () => {
+    userQuestion.classList.remove(corAplicada)
+    userQuestion.classList.add('orange')
+    corAplicada = 'orange'
+})
+
+btnAzul.addEventListener('click', () => {
+    userQuestion.classList.remove(corAplicada)
+    userQuestion.classList.add('blue')
+    corAplicada = 'blue'
+})
+
+btnVerde.addEventListener('click', () => {
+    userQuestion.classList.remove(corAplicada)
+    userQuestion.classList.add('green')
+    corAplicada = 'green'
+})
